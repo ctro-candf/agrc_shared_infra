06f794776cc45d66652795c8931fa16354e0a180
Cezary Trojanowski <ctro@macbookpro.home>
2025-06-05 09:08:55 +0200

    updated db variables

--- variables.tf
+++ variables.tf
@@ -1,7 +1,11 @@
 variable "project_name" {}
 variable "environment" {}
-variable "db_username" {}
-variable "db_password" {}
+variable "db_username" {
+    sensitive   = true
+}
+variable "db_password" {
+    sensitive   = true
+}
 variable "shared_vpc_cidr" {}
 variable "private_subnet_cidrs" {}
 variable "database_subnet_cidrs" {}
